33b6ec8fd00a175297eae6d594e77f95210155d3
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-03-21 16:07:49 +0100

    Two other code challenges solved

---
+++ RelativeSortArray.py
@@ -0,0 +1,43 @@
+"""
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+
+Example 1:
+
+Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+Output: [2,2,2,1,4,3,3,9,6,7,19]
+
+Constraints:
+
+    1 <= arr1.length, arr2.length <= 1000
+    0 <= arr1[i], arr2[i] <= 1000
+    All the elements of arr2 are distinct.
+    Each arr2[i] is in arr1.
+
+Source: LeetCode.com
+"""
+
+from typing import List
+
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        sorted_arr1 = []
+        for el in arr2:
+            indicies = [i for i,j in enumerate(arr1) if j == el]
+            n = 0
+            for id in indicies:
+                value = arr1.pop(id - n)
+                n += 1
+                sorted_arr1.append(value)
+
+        arr1.sort()
+        sorted_arr1.extend(arr1)
+
+        return sorted_arr1
+
+
+s = Solution()
+print(s.relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]))
